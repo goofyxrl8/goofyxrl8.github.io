e87ebc16c46058bb36dc943f1e30c68c19bbca93
goofyxrl8 <74901073+goofyxrl8@users.noreply.github.com>
2020-11-23 05:58:49 -0600

    Update CtrlSaludo.js

--- js/CtrlSaludo.js
+++ js/CtrlSaludo.js
@@ -4,6 +4,6 @@ export class CtrlSaludo {
   calculaSaludo(nombre) {
     const nombreSinEspacios = nombre ? nombre.trim() : "";
     valida(nombreSinEspacios, "Falta proporcionar el nombre.");
-    return `Bienvenido a la plataforma de Git ${nombreSinEspacios}.`;
+    return `Bienvenido${nombreSinEspacios}.`;
   }
 }
